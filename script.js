c99c4db3789472529f374eac5264d37319bf9559
mARK <72921963+markahd@users.noreply.github.com>
2024-04-13 11:11:25 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ async function convertToMp3() {
     const resultDiv = document.getElementById("result");
 
     try {
+        if (typeof ytdl === 'undefined') {
+            await loadScript('https://cdn.jsdelivr.net/npm/ytdl-core@latest');
+        }
+
         const info = await ytdl.getInfo(youtubeUrl);
         const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
         const audioStream = ytdl.chooseFormat(audioFormats, { quality: 'highestaudio' });
@@ -23,11 +27,12 @@ async function convertToMp3() {
     }
 }
 
-// Import ytdl-core library dynamically
-const script = document.createElement('script');
-script.src = 'https://cdn.jsdelivr.net/npm/ytdl-core@latest';
-script.onload = function () {
-    console.log('ytdl-core loaded.');
-    // Now ytdl-core is loaded, you can call functions that use it
-};
-document.head.appendChild(script);
+function loadScript(url) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = url;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.head.appendChild(script);
+    });
+}
